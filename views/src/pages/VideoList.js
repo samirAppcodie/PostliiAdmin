b76599eb0670a7d03e94abfce73187380e867951
samirappcodie <samir.appcodie@gmail.com>
2025-10-10 17:55:34 +0530

    Overwrite old code with latest local version1

--- views/src/pages/VideoList.js
+++ views/src/pages/VideoList.js
@@ -4,7 +4,6 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const VideoList = () => {
-  //test
   const [groupedVideos, setGroupedVideos] = useState({});
   const [categoryPages, setCategoryPages] = useState({});
   const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
